225c2de499b2f323df5bc2878abac56991853601
Steph <steph@Stephs-MacBook-Pro.local>
2020-06-12 12:58:06 -0400

    adding routes to the api

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 
+const productRoutes = require('./api/routes/products');
+
 
 //use methos sets up middlewear
 //an incoming request has to go through app.use and whatever pass to it
-app.use((req, res, next)=> {
-    res.status(200).json({
-        message: 'It works'
-    });
-});
+app.use('/products', productRoutes);
 
 module.exports = app;
