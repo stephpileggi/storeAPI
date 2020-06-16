b71e71449ccbbfd8b167777ec9a8be25bbee1633
Steph <steph@Stephs-MacBook-Pro.local>
2020-06-16 13:51:46 -0400

    Adding MongoDB and Mongoose and handling endpoint

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+//connect to the mongodb path and change the password to whatever it was in mongodb
+mongoose.connect('mongodb+srv://node_shop:' + process.env.MONGO_ATLAS_PW + '@cluster0-zlfte.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    });
 
 
 app.use(morgan('dev'));
